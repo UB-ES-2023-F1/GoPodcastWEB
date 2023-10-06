9fc7595ff74704d48c8967d58dc6a340ec7f4f73
Alba Chicharro <achichga7@alumnes.ub.edu>
2023-10-06 15:59:28 +0200

    Implemented Register view (white)

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,22 @@ name: 'App'
 
 <style>
 #app {
-font-family: 'Avenir', Helvetica, Arial, sans-serif;
+/* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
 -webkit-font-smoothing: antialiased;
 -moz-osx-font-smoothing: grayscale;
-text-align: center;
+/* text-align: center; */
 color: #2c3e50;
 margin-top: 0px;
+}
+
+#slogan{
+  color: rgb(119, 0, 255);
+  font-style: italic;
+  font-family: 'Lucida Sans Unicode';
+  font-weight: 800;
+}
+
+a{
+  color: rgb(119, 0, 255);
 }
 </style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,27 +1,48 @@
 <template>
-    <div class="register">
-        <h1>Sign Up</h1>
-        <button class="btn btn-success" @click="">Bootstrap test</button>
-        <form @submit.prevent="submitForm">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" required>
+    <div class="register container mt-5 text-black w-50">
+        <div class="logo d-flex justify-content-center">
+            <img src="../assets/fake-logo.png" alt="logo" :style="{width: '30px', height: '30px'}">
+            <h5 class="pt-1 ms-2 fw-bold">GoPodcast</h5>
+        </div>
+        <h5 id="slogan" class="text-center mt-5 f-italic" style="font-size: 1.8rem;">Podcasts, Play, Repeat!</h5>
+        <!-- <div class="border-bottom my-5 col-xl-7 mx-auto border-2"></div> -->
+        <p class="text-center mt-5 fw-bold" :style="{fontSize: '1.2rem'}">Sign up to listen to your favorite podcasts</p>
+        <form @submit.prevent="submitForm" class="register-form col-xl-7 mx-auto mt-4">
+            <!-- <div class="form-group mb-2">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Username" required>
+            </div> -->
+            <div class="form-group left mb-2">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
             </div>
-            <div class="form-group">
+            <div class="form-group mb-2">
                 <label for="password">Password</label>
-                <input type="password" id="password" required>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+                <small id="pswHelp" class="form-text text-muted">The password must be blablabla.</small>
             </div>
-            <div class="form-group">
-                <label for="confirm-password">Confirm Password</label>
-                <input type="password" id="confirm-password" required>
+            <div class="form-group mb-2">
+                <label for="confirmPassword">Confirm password</label>
+                <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm password" required>
             </div>
-            <button type="submit">Sign Up</button>
+            <!-- <div class="form-group mb-2">
+                <label for="birthdate">Birthdate</label>
+                <input type="date" class="form-control" id="birthdate" placeholder="Birthdate" required>
+            </div> -->
+            
+            <div class="justify-content-center d-flex mt-3">
+                <button type="submit" class="btn-submit btn btn-dark mt-3">Sign Up</button>
+            </div>
         </form>
+        <div class="d-flex justify-content-center mt-4">
+            <p class="text-center">Already have an account? <router-link to="/login">Log in</router-link></p>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    name: 'Register',
     data() {
         return {
             email: '',
@@ -41,4 +62,5 @@ export default {
 </script>
 
 <style>
+
 </style>
