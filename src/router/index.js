0e8d155ba13fdd5c3e86fe7c7729df3a78cc1329
OscarJaquero <oscarsanchezjaqueror@gmail.com>
2023-10-09 18:37:24 +0200

    Unión de sing un con log in y añadimos nueva ruta /login en index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Register from '../views/Register.vue';
+import Login from '../views/Login.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Register',
     component: Register
+  },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: Login
   }
 ];
 
---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="login">
+        <div class="container pt-5 w-50">
+            <div class="logo d-flex justify-content-center">
+                <router-link to="/GoPodcast" class="d-flex align-items-center" style="text-decoration: none; align-items:normal;">
+                <img src="../assets/logo_white.png" alt="logo" :style="{width: '25px', height: '25px'}">
+                <h5 class="ms-2 fw-lighter text-white" style="font-size: 1.3rem;">GoPodcast</h5>
+                </router-link>
+            </div>
+            <h5 id="slogan" class="text-center mt-5 f-italic" style="font-size: 1.8rem;">Podcasts, Play, Repeat!</h5>
+            <p class="text-center mt-5 fw-bold" :style="{fontSize: '1.2rem'}">Log in to listen to your favorite podcasts</p>
+            
+        </div>
+    </div>
+</template>
