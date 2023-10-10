2f83dd9a7fb0b37d4683f7b3a8b23d28d6c01ee5
Alba Chicharro <achichga7@alumnes.ub.edu>
2023-10-10 19:26:11 +0200

    fix: Updated dessign and email-psw validation

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,69 +1,149 @@
 <template>
     <div class="register">
-    <div class="container pt-5 w-50">
-        <div class="logo d-flex justify-content-center">
-            <router-link to="/GoPodcast" class="d-flex align-items-center" style="text-decoration: none; align-items:normal;">
-            <img src="../assets/logo_white.png" alt="logo" :style="{width: '25px', height: '24px'}">
-            <h5 class="ms-2 fw-lighter text-white" style="font-size: 1.3rem;">GoPodcast</h5>
-            </router-link>
-        </div>
-        <h5 id="slogan" class="text-center mt-5 f-italic" style="font-size: 1.8rem;">Podcasts, Play, Repeat!</h5>
-        <!-- <div class="border-bottom my-5 col-xl-7 mx-auto border-2"></div> -->
-        <p class="text-center mt-5 fw-bold" :style="{fontSize: '1.2rem'}">Sign up to listen to your favorite podcasts</p>
-        <form @submit.prevent="submitForm" class="register-form col-xl-7 mx-auto mt-4">
-            <!-- <div class="form-group mb-2">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" placeholder="Username" required>
-            </div> -->
-            <div class="form-group left mb-2">
-                <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+        <div class="container pt-5 w-50">
+            <div class="logo d-flex justify-content-center">
+                <router-link to="/" class="d-flex align-items-center" style="text-decoration: none; align-items:normal;">
+                    <img src="../assets/logo_white.png" alt="logo" :style="{ width: '25px', height: '24px' }">
+                    <h5 class="ms-2 fw-lighter text-white" style="font-size: 1.3rem;">GoPodcast</h5>
+                </router-link>
             </div>
-            <div class="form-group mb-2">
-                <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="Password">
-                <small id="pswHelp" class="opacity-50">The password must be blablabla.</small>
+            <h5 id="slogan" class="text-center mt-5 f-italic" style="font-size: 1.8rem;">Play, Enjoy, Repeat!</h5>
+            <p class="text-center mt-5 fw-bold" :style="{ fontSize: '1.2rem' }">Sign up to listen to your favorite podcasts
+            </p>
+            <form @submit.prevent="submitForm" class="register-form col-xl-7 mx-auto mt-4">
+                <div class="form-group left mb-2">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                        placeholder="Enter email" v-model="email" required>
+                </div>
+                <div class="form-group mb-2">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Password" v-model="password"
+                        required>
+                    <small id="pswHelp" class="opacity-50" v-if="this.password">
+                        Password must be at least 8-20 characters long, contain at least 1 big letter, 1 small letter and 1
+                        number</small>
+                </div>
+                <div class="form-group mb-2">
+                    <label for="confirmPassword">Confirm password</label>
+                    <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm password"
+                        v-model="confirmPassword" required>
+                </div>
+                <div class="justify-content-center d-flex mt-3">
+                    <button type="submit" class="btn-submit btn btn-dark mt-3" @click="onSubmit">Sign Up</button>
+                </div>
+            </form>
+            <div class="d-flex justify-content-center mt-4">
+                <p class="text-center">Already have an account? <router-link to="/login">Log in</router-link></p>
             </div>
-            <div class="form-group mb-2">
-                <label for="confirmPassword">Confirm password</label>
-                <input type="password" class="form-control" id="confirmPassword" placeholder="Confirm password" required>
-            </div>
-            <!-- <div class="form-group mb-2">
-                <label for="birthdate">Birthdate</label>
-                <input type="date" class="form-control" id="birthdate" placeholder="Birthdate" required>
-            </div> -->
-            
-            <div class="justify-content-center d-flex mt-3">
-                <button type="submit" class="btn-submit btn btn-dark mt-3">Sign Up</button>
-            </div>
-        </form>
-        <div class="d-flex justify-content-center mt-4">
-            <p class="text-center">Already have an account? <router-link to="/login">Log in</router-link></p>
         </div>
     </div>
-</div>
 </template>
 
-<style>
-
-</style>
+<style></style>
 
 <script>
+// import axios from 'axios'
+
 export default {
     name: 'Register',
     data() {
         return {
-            email: '',
-            password: '',
-            confirmPassword: ''
+            email: null,
+            password: null,
+            confirmPassword: null
         }
     },
     methods: {
-        submitForm() {
-            if (this.password !== this.confirmPassword) {
-                alert('Passwords do not match')
-                return
+        validatePassword() {
+            // The password must be at least 8 characters long
+            if (this.password.length < 8 || this.password.length > 20) {
+                alert('Password must be at least 8-20 characters long')
+                document.getElementById('password').style.borderColor = 'red'
+                document.getElementById('confirmPassword').style.borderColor = 'red'
+                return false
             }
+            // The fields must not be empty
+            else if (this.password === '' || this.confirmPassword === '') {
+                alert('Fields must not be empty')
+                document.getElementById('password').style.borderColor = 'red'
+                document.getElementById('confirmPassword').style.borderColor = 'red'
+                return false
+            }
+            // The password must have at least 1 big letter
+            else if (!/[A-Z]/.test(this.password)) {
+                alert('Password must have at least 1 big letter')
+                document.getElementById('password').style.borderColor = 'red'
+                document.getElementById('confirmPassword').style.borderColor = 'red'
+                return false
+            }
+            // The password must have at least 1 small letter
+            else if (!/[a-z]/.test(this.password)) {
+                alert('Password must have at least 1 small letter')
+                document.getElementById('password').style.borderColor = 'red'
+                document.getElementById('confirmPassword').style.borderColor = 'red'
+                return false
+            }
+            // The password must have at least 1 number
+            else if (!/[0-9]/.test(this.password)) {
+                alert('Password must have at least 1 number')
+                document.getElementById('password').style.borderColor = 'red'
+                document.getElementById('confirmPassword').style.borderColor = 'red'
+                return false
+            }
+            // Both password and confirm password must be the same
+            else if (this.password !== this.confirmPassword) {
+                alert('Passwords do not match')
+                document.getElementById('password').style.borderColor = 'red'
+                document.getElementById('confirmPassword').style.borderColor = 'red'
+                return false
+            }
+            else {
+                document.getElementById('password').style.borderColor = 'green'
+                document.getElementById('confirmPassword').style.borderColor = 'green'
+            }
+            return true
+        },
+        validateEmail() {
+            // The email must not be empty
+            if (this.email === '') {
+                alert('Email must not be empty')
+                document.getElementById('email').style.borderColor = 'red'
+                return false
+            }
+            // The email must have email format
+            else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email)) {
+                alert('Incorrect format')
+                document.getElementById('email').style.borderColor = 'red'
+                return false
+            }
+            else {
+                document.getElementById('email').style.borderColor = 'green'
+            }
+            return true
+        },
+        onSubmit() {
+            if (this.validatePassword() && this.validateEmail()){
+                const parameters = {
+                    email: this.email,
+                    password: this.password
+                }
+                console.log(parameters)
+                //   const path = 'http://localhost:5117/account' // Descomentar y modificar por el endpoint correcto
+                //   axios.post(path, parameters)
+                //     .then((res) => {
+                //       alert('Account created')
+                //       this.backToLogin()
+                //     })
+                //     .catch((error) => {
+                //       alert('Error creating account')
+                //       console.error(error)
+                //     })
+            }
+            
+        },
+        backToLogin() {
+            this.$router.push('/login')
         }
     }
 }
