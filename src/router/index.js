89d2982ec7cf27049a2401cd12271a6c47965b72
OscarJaquero <oscarsanchezjaqueror@gmail.com>
2023-10-17 22:35:12 +0200

    Arreglo bug index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
-    path: '/register',
+    path: '/',
     name: 'Register',
     component: () => import('../views/Register.vue')
   },
@@ -15,7 +15,7 @@ const routes = [
   {
     path: '/publish',
     name: 'PublishPodcast',
-    component: () => import('../views/Publish.vue')
+    component: () => import('../views/PublishPodcast.vue')
   }
 ]
 
