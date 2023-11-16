d90e032859a8903afc9fcf5c51f34c7d319e7063
OscarJaquero <oscarsanchezjaquero@gmail.com>
2023-11-16 16:53:39 +0100

    Mods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -222,3 +222,20 @@ h2 {
   color: #000;
 }
 </style>
+
+<!--import { createApp } from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+
+const app = createApp(App)
+
+export default new Vuex.Store({
+  state: {
+    userIsLoggedIn: false,
+  },
+  mutations: {
+    setUserIsLoggedIn(state, value) {
+      state.userIsLoggedIn = value
+    },
+  },
+})-->
