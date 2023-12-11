a60fa2838a96fffec1a0f5461be9f2fbee73e95e
Alba Chicharro <90827999+alba-ch@users.noreply.github.com>
2023-12-11 12:34:46 +0100

    Updated text

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -11,49 +11,31 @@
                 </div>
                 <div class="content">
                     <div class="description">
-                        <h1 class="text-center mb-3">About Us</h1>
-                        <p>Welcome to our website! We are a team of passionate individuals dedicated to bringing you the
-                            best
-                            podcasts
-                            on the web.</p>
-                        <p>Our mission is to provide a platform where podcasters can share their stories, ideas, and
-                            expertise
-                            with the
-                            world. Whether you're a podcast enthusiast or a content creator, we hope you find our website
-                            informative,
-                            entertaining, and inspiring.</p>
+                        <h1 class="text-center mb-3">Sobre GoPodcast</h1>
+                        <p>Gopodcast nace del anhelo de cinco informáticos de hacer de este mundo un lugar mejor: un lugar en el que el conocimiento circule libremente a través del podcasting. Descubre con nosotros una comunidad de oyentes y locutores que toca todo tipo de temas: desde la ciencia hasta el arte, desde el entretenimiento hasta la salud y el fitness, desde la historia hasta la actualidad...</p>
+                        <p >Nuestro objetivo: <span style="text-decoration: underline;">convertirnos en la Wikipedia de los podcasts.</span></p>
                     </div>
                     <div class="images-and-text mt-5">
-                        <h4 style="color: rgb(85, 50, 255); text-shadow: 0px 0px 1px rgb(147, 64, 255);">More about</h4>
+                        <h4 style="color: rgb(85, 50, 255); text-shadow: 0px 0px 1px rgb(147, 64, 255);">
+                            Estos son nuestros valores</h4>
                         <div class="row mt-4">
                             <div class="col-md-6">
                                 <img src="../assets/redpanda.jpg" alt="about-us" class="img-fluid"
                                     style="width: 100%; max-height: 300px;">
                             </div>
                             <div class="col-md-6">
-                                <h5 style="color: rgb(85, 50, 255);">What is GoPodcast?</h5>
-                                <p>Our website is a place where you can find podcasts on any topic imaginable. We have
-                                    podcasts
-                                    about
-                                    sports, politics, business, and more! You can also listen to your favorite podcasters
-                                    talk
-                                    about
-                                    their lives or share their thoughts on current events.</p>
+                                <h5 style="color: rgb(85, 50, 255);">Contenido</h5>
+                                <p>Nuestra plataforma permite a cualquier usuario compartir sus experiencias y conocimiento. Todos somos oyentes y creadores, sin distinción alguna. Creemos en la riqueza de la diversidad de temas y en la libertad de expresión para que podamos tratar cualquier tema, ¡sin tabúes!</p>
 
                                 <!-- BUtton to Explore -->
-                                <button class="btn mt-3" @click="$router.push('/');">Explore Home</button>
+                                <button class="btn mt-3" @click="$router.push('/');">Explorar el Home</button>
                             </div>
                         </div>
                         <div class="row mt-5">
                             <div class="col-md-6">
-                                <h5 style="color: rgb(85, 50, 255);">Who are we?</h5>
-                                <p>Lorem ipsum dolor sit ame consectetur adipi sicing elit. Quisquam, voluptatem. Lorem
-                                    ipsum
-                                    dolor sit
-                                    amet consectetur adipisicing elit. Quisquam, voluptatem. Lorem ipsum dolor sit amet
-                                    consectetur
-                                    adipisicing elit. Quisquam, voluptatem. </p>
-                                <button class="btn mt-3" @click="$router.push('/');">See Podcast</button>
+                                <h5 style="color: rgb(85, 50, 255);">Accesibilidad</h5>
+                                <p>De nada sirve tener un buen contenido si este no llega a todo el mundo. Por eso nuestro modelo es totalmente gratuito, tanto para escuchar como para publicar. Funcionamos a través de donaciones de nuestros generosos usuarios. Así que si confías en este proyecto, no dudes en aportar tu granito de arena.</p>
+                                <button class="btn mt-3" @click="$router.push('/');">Ver Podcasts</button>
                             </div>
                             <div class="col-md-6">
                                 <img src="../assets/redpanda.jpg" alt="about-us" class="img-fluid"
@@ -66,17 +48,30 @@
                                     style="width: 100%; max-height: 300px;">
                             </div>
                             <div class="col-md-6">
-                                <h5 style="color: rgb(85, 50, 255);">Publish your episodes!</h5>
-                                <p>Lorem ipsum dolor sit ame consectetur adipi sicing elit. Quisquam, voluptatem. Lorem
-                                    ipsum
-                                    dolor sit
-                                    amet consectetur adipisicing elit. Quisquam, voluptatem. Lorem ipsum dolor sit amet
-                                    consectetur
-                                    adipisicing elit. Quisquam, voluptatem.</p>
+                                <h5 style="color: rgb(85, 50, 255);">Transparencia</h5>
+                                <p>¡Lo que ves es lo que hay! No comercializamos con tus datos ni usamos cookies para rastrear tu comportamiento en la web. Nos comprometemos a ser abiertos y honestos en cuanto a nuestras políticas, prácticas y decisiones.</p>
 
                                 <!-- BUtton to Explore -->
-                                <button class="btn mt-3" @click="$router.push('/');">Publish Episode</button>
+                                <button class="btn mt-3" @click="$router.push('/');">Publicar Episodio</button>
                             </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col-md-6">
+                                <h5 style="color: rgb(85, 50, 255);">Comunidad</h5>
+                                <p>Gopodcast no solo es un medio de comunicación, sino un vibrante punto de encuentro para mentes curiosas y voces elocuentes. La interacción con el resto de usuarios es un aspecto clave en el que queremos trabajar. Sigue a tus creadores favoritos para estar al día de sus últimas publicaciones y conecta con ellos comentando en sus episodios.</p>
+                                <button class="btn mt-3" @click="$router.push('/');">Descubre artistas</button>
+                            </div>
+                            <div class="col-md-6">
+                                <img src="../assets/redpanda.jpg" alt="about-us" class="img-fluid"
+                                    style="width: 100%; max-height: 300px;">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="slogan">
+                        <h1 class="text-center mt-5">GoPodcast</h1>
+                        <p class="text-center">Un lugar en el que descubrir, aprender y compartir.</p>
+                        <div class="d-flex justify-content-center">
+                            <button class="btn btn-secondary mt-3" @click="$router.push('/register');">Sign Up</button>
                         </div>
                     </div>
                 </div>
